6f0836848f16a9cebdfc5e3af3ded8ac16b654dc
Matias Niemelä <matias@yearofmoo.com>
2014-04-24 18:48:35 -0400

    chore(angularSpec): add missing spec symbols

--- test/angular_spec.dart
+++ test/angular_spec.dart
@@ -229,7 +229,6 @@ main() {
         "angular.formatter_internal.OrderBy",
         "angular.formatter_internal.Stringify",
         "angular.formatter_internal.Uppercase",
-        "angular.introspection.getTestability",
         "angular.introspection.ngDirectives",
         "angular.introspection.ngInjector",
         "angular.introspection.ngProbe",
